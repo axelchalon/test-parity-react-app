e9eb2bff362a3a30aaccd3b9ae726b1d26466716
Nicolas Gotchac <ngotchac@gmail.com>
2018-04-13 16:04:17 +0200

    Fix initial setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('ok!');
+import React from 'react';
+
+export default class App extends React.Component {
+  render () {
+    return (
+      <div>
+        Salut !
+      </div>
+    );
+  }
+}
